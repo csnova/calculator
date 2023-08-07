ecce135a76cc7754c2096642b66b68e1740fd72d
csnova <chandlersnova@gmail.com>
2023-08-07 14:48:51 -0700

    Add Function and Style In HTML added containers and buttons for UI. In CSS added style to make look like a calculator. In JS added function for each type of operation I wanted to run, then added event listeners so those functions would run when buttons are pressed.

---
+++ script.js
@@ -0,0 +1,116 @@
+let firstNum = 0;
+let secNum = 0;
+let numIndicator = "first";
+let operator;
+let displayValue = "Example Text";
+
+const add = function (a, b) {
+  let numA = Number(a);
+  let numB = Number(b);
+  let total = numA + numB;
+  return total;
+};
+
+const subtract = function (a, b) {
+  let numA = Number(a);
+  let numB = Number(b);
+  let total = numA - numB;
+  return total;
+};
+
+const multiply = function (a, b) {
+  let numA = Number(a);
+  let numB = Number(b);
+  let total = numA * numB;
+  return total;
+};
+
+const divide = function (a, b) {
+  let numA = Number(a);
+  let numB = Number(b);
+  let total = numA / numB;
+  return total;
+};
+
+const operate = function (firstNum, secNum, operator) {
+  let solution;
+  if (operator == "+") {
+    solution = add(firstNum, secNum);
+    solution = Math.round(solution * 100) / 100;
+    return solution;
+  }
+  if (operator == "-") {
+    solution = subtract(firstNum, secNum);
+    solution = Math.round(solution * 100) / 100;
+    return solution;
+  }
+  if (operator == "/") {
+    solution = divide(firstNum, secNum);
+    solution = Math.round(solution * 100) / 100;
+    return solution;
+  }
+  if (operator == "*") {
+    solution = multiply(firstNum, secNum);
+    solution = Math.round(solution * 100) / 100;
+    return solution;
+  }
+};
+
+function display(value) {
+  displayValue = value;
+  const div = document.querySelector("div");
+  const screen = document.querySelector(".screen");
+  const para = document.createElement("p");
+  para.classList.add("para");
+  para.textContent = displayValue;
+  screen.appendChild(para);
+}
+
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  if (button.className === "num") {
+    button.addEventListener("click", () => {
+      if (numIndicator === "first") {
+        firstNum += button.id;
+        display(button.id);
+      }
+      if (numIndicator === "second") {
+        secNum += button.id;
+        display(button.id);
+      }
+    });
+  }
+  if (button.className === "op") {
+    button.addEventListener("click", () => {
+      if (numIndicator === "first") {
+        firstNum = Number(firstNum);
+        numIndicator = "second";
+        operator = button.id;
+        display(button.id);
+      }
+    });
+  }
+  if (button.className === "equal") {
+    button.addEventListener("click", () => {
+      secNum = Number(secNum);
+      numIndicator = "NA";
+      display(button.id)
+      display(operate(firstNum, secNum, operator));
+    });
+  }
+  if (button.className === "clr") {
+    button.addEventListener("click", () => {
+      firstNum = 0;
+      secNum = 0;
+      numIndicator = "first";
+      removeElementsByClass("para");
+    });
+  }
+});
